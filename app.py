b9fcfbc1b766e8b5e9bd7b48e7e23d68ed0c9119
xavier-fenton <xavierfenton99@gmail.com>
2022-12-08 08:51:47 +1300

    create runApps button

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#2
 
 openFile.pack()
 
-# runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42")
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42")
 
-# runApps.pack()
+runApps.pack()
 
 root.mainloop()
