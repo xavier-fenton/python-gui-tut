60ef85c28e53b39fe508c464dd1b4a81e3bfcda1
xavier-fenton <xavierfenton99@gmail.com>
2022-12-08 09:48:39 +1300

    finish tutorial, first python app

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import os
 root = tk.Tk()
 apps= []
 
+if os.path.isfile('save.txt'): # when adding files, then closing the app this saves the file as save.txt. Which then stores the files to the app. Each time the file is opened, it will show our saved apps. 
+  with open('save.txt', 'r') as f:
+    tempApps = f.read()
+    tempApps = tempApps.split(',') #removes comma
+    apps = [x for x in tempApps if x.strip()] #removes empty spaces
+
 
 def addApp():
   for widget in frame.winfo_children(): #deletes old widgets
@@ -36,4 +42,14 @@ runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263
 
 runApps.pack() #this adds the button to the apps
 
+
+for app in apps:
+  label = tk.Label(frame, text=app) #displays our applications from save.txt
+  label.pack()
+
+
 root.mainloop()
+
+with open('save.txt', 'w') as f:
+  for app in apps:
+    f.write(app + ',')
