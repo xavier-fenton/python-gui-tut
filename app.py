94d5755f92a386ab322409b7c11d70e1ff088501
xavier-fenton <xavierfenton99@gmail.com>
2022-12-08 08:53:22 +1300

    addApp function

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ root = tk.Tk()
 # apps= []
 
 
-# def addApp():
-#   filename= filedialog.askopenfilename(initialdir="/", title="Select a File", 
-#                                         filetypes=(("executables", "*.exe"), ("All files", "*.*")))
+def addApp():
+  filename= filedialog.askopenfilename(initialdir="/", title="Select a File", 
+                                        filetypes=(("executables", "*.exe"), ("All files", "*.*")))
   # apps.append(filename)
   # print(filename)
   # for app in apps:
@@ -22,7 +22,7 @@ canvas.pack()
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp) # open file with command = addApp function
 
 openFile.pack()
 
